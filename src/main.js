c42f8ac5a0934b98fd13d23eff4fd0a159bfa438
Desarrollo Myz <mylittleviruz@gmail.com>
2022-07-21 20:50:58 -0300

    Terminando la tarjeta

---
+++ src/main.js
@@ -0,0 +1,17 @@
+console.log('first')
+import './style.css'
+/**
+ * npm init vite my-proyect (YA)
+ * cd my-proyect  (YA)
+ * 
+ * INSTALAR TAILWIND CSS
+ * 
+ * npm install -D tailwindcss postcss autoprefixer
+ * npx tailwindcss init -p
+ * 
+ * CREA 2 ARCHIVOS --AGREGAMOS EN tailwind.config.cjs
+ * 
+ * "./index.html",
+    "./src/** / *.{vue,js,ts,jsx,tsx}",
+ * 
+ */
